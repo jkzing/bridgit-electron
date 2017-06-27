8ff4bded099b57c672bca4810c597da15c38e22b
jkzing <jingkai.zhao@foxmail.com>
2017-06-28 00:00:27 +0800

    fix build with ts

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import { Vue, Component } from 'av-ts'
 
 @Component
---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+declare module '*.vue' {
+  import * as Vue from 'vue'
+  const Component: typeof Vue
+  export default Component
+}
