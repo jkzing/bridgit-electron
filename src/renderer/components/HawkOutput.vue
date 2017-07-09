cd9ffaf12acf12eae517ed7829b86b5aaaded6eb
jkzing <jingkai.zhao@foxmail.com>
2017-07-09 21:10:05 +0800

    add HawkOutput view

---
+++ src/renderer/components/HawkOutput.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+  name: 'HawkOutput'
+})
+export default class HawkOutput extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
+
