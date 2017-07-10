bd243abdbdcf5d595810f82d6c28755e214ec1c2
jkzing <jingkai.zhao@foxmail.com>
2017-07-10 22:53:46 +0800

    feat: add detail to HawkOutput view

--- src/renderer/components/HawkOutput.vue
+++ src/renderer/components/HawkOutput.vue
@@ -1,5 +1,54 @@
 <template>
-  <div>output</div>
+  <div>
+    <template v-if="config">
+      <v-layout row align-center>
+        <v-flex xs2>
+          <p>Name: </p>
+        </v-flex>
+        <v-flex xs4>
+          <p>{{ name }}</p>
+        </v-flex>
+      </v-layout>
+      <v-layout row align-center>
+        <v-flex xs2>
+          <p>ID: </p>
+        </v-flex>
+        <v-flex xs4>
+          <p>{{ config.id }}</p>
+        </v-flex>
+        <v-flex xs2>
+          <p>Key: </p>
+        </v-flex>
+        <v-flex xs4>
+          <p>{{ config.key }}</p>
+        </v-flex>
+      </v-layout>
+      <v-card class="mt-1">
+        <div style="height: 500px">
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline white--text">hehe</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </div>
+        <v-divider></v-divider>
+        <v-card-actions class="instance-actions white">
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>favorite</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>bookmark</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>share</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </div>
 </template>
 
 <script lang="ts">
@@ -27,7 +76,11 @@ export default class HawkOutput extends Vue {
 </script>
 
 <style lang="scss">
-
+.instance-actions {
+  padding: 8px 4px;
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
 
 
